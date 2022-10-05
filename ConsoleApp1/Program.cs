ce35f24276229a4c2fa079607733592caa8f9540
naslanzade <novrastafa@code.edu.az>
2022-10-05 22:40:18 +0400

    add

---
+++ ConsoleApp1/Program.cs
@@ -0,0 +1,49 @@
+﻿using System;
+
+namespace ConsoleApp1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+           
+            //{
+            //    int num = 16;
+            //    int count = 0;
+
+            //    if (num == 1 || num == 0)
+            //    {
+            //        Console.WriteLine("neither prime nor complex numbers");
+            //    }
+            //    else
+
+            //    {
+            //        for (int i = 1; i <= num; i++)
+            //        {
+            //            if (num % i == 0)
+            //            {
+            //                count++;
+            //            }
+            //        }
+            //        if (count > 2)
+
+            //        {
+            //            Console.WriteLine("Complex numbers");
+
+            //        }
+
+            //        else
+            //        {
+            //            Console.WriteLine("Prime numbers");
+            //        }
+            //    }
+            //}
+
+
+
+
+
+
+        }
+    }
+}
